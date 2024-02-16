0c186779f37b7a723ee5403d1e5060046940101a
lburnet <lburnet@student.42lyon.fr>
2024-02-16 17:07:50 +0100

    03 ok

--- rust-book/03-Common Programming Concepts/variables/src/main.rs
+++ rust-book/03-Common Programming Concepts/variables/src/main.rs
@@ -135,7 +135,7 @@ fn main() {
 }*/
 
 // from 3.3
-fn main() {
+/*fn main() {
 	// println!("Hello, world!");
 
 	// another_function();
@@ -178,4 +178,91 @@ fn five() -> i32 { // '->' is needed to declare the type of the returned value
 
 fn plus_one(x: i32) -> i32 {
 	x + 1
+}*/
+
+// from 3.5
+
+fn main() {
+	// let number = 7;
+
+	// if number < 5 { // condition must be a bool or else we've got an error
+	// 	println!("condition was true");
+	// } else {
+	// 	println!("condition was false");
+	// }
+
+	// if number % 4 == 0 {
+	// 	println!("number is divisible by 4");
+	// } else if number % 3 == 0 {
+	// 	println!("number is divisible by 3");
+	// } else if number % 2 == 0 {
+	// 	println!("number is divisible by 2");
+	// } else {
+	// 	println!("number is not divisible by 4, 3, or 2");
+	// }
+
+	// let condition = false;
+	// let number = if condition { 5 } else { 6 }; // here the 2 expressions must be the same type or we get an error
+	// println!("The value of number is: {number}");
+	
+	// loop { // infinite loop because no "break"
+	// 	println!("again!");
+	// }
+
+	// let mut counter = 0;
+	// let result = loop {
+	// 	counter += 1;
+
+	// 	if counter == 10 {
+	// 		break counter * 2; // ';' is technically optional, "break" is very similar to "return"
+	// 	}
+	// };
+	// println!("The result is {result}");
+
+	// let mut count = 0;
+	// 'counting_up: loop {
+	// 	println!("count = {count}");
+	// 	let mut remaining = 10;
+
+	// 	loop {
+	// 		println!("remaining = {remaining}");
+	// 		if remaining == 9 {
+	// 			break;
+	// 		}
+	// 		if count == 2 {
+	// 			break 'counting_up;
+	// 		}
+	// 		remaining -= 1;
+	// 	}
+
+	// 	count += 1;
+	// }
+	// println!("End count = {count}");
+
+	// let mut number = 3;
+	// while number != 0 {
+	// 	println!("{number}!");
+
+	// 	number -= 1;
+	// }
+	// println!("LIFTOFF!!!");
+
+	// let a = [10, 20, 30, 40, 50];
+	// let mut index = 0;
+	// while index < 5 { // like 'for' after this
+	// 	println!("the value is: {}", a[index]);
+
+	// 	index += 1;
+	// }
+
+	// let a = [10, 20, 30, 40, 50];
+	// for element in a { // like "while" above
+	// 	println!("the value is: {element}");
+	// }
+
+	for number in (1..4).rev() { // in range (inclued..exclued)
+		println!("{number}!");
+	}
+	println!("LIFTOFF!!!");
 }
+
